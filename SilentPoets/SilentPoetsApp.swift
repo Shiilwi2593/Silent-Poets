6a898ffbd6fa56feab5bb4724e5a026d563f1962
graylwi2593 <graylwi2593@gmail.com>
2024-10-14 16:12:28 +0700

    Initial Commit

---
+++ SilentPoets/SilentPoetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilentPoetsApp.swift
+//  SilentPoets
+//
+//  Created by Trịnh Kiết Tường on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SilentPoetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
